04fd2bdb4838cd778ed8367d5d7965d3583aa287
AWinterman <andywinterman@gmail.com>
2013-04-21 23:41:59 -0700

    added new functionality and comments.
    Need to figure out how to npm publish
    without including all of docs???

--- Mapping.js
+++ Mapping.js
@@ -1,62 +1,160 @@
+// # d3-mapping.js #
+//
+// Mapping handles all the book-keeping required to map points 
+// from the data space into coordinates of screen space.
+
+''
+
+// ## Exports the constructor function.
+//  Usage: 
+// ```js
+// var mapping = new Mapping(d3.scale.linear(), accessor)
+//
+// function accessor(d){ 
+//   return +d.dollars + (+d.cents)/100
+// } 
+// ```
 module.exports = Mapping
 
+// Depends on [d3.js](http://d3js.org/)
 var d3 = require("d3")
 
-function Mapping(scale, accessor){
-  //defines a base object which handles all the information around mapping from
-  //the data space to the screen space
 
-  //map `this` to a variable so we can refer to it inside of other function
-  //closures
+// In the constructor I like to
+// establish the shape of the class, and do any work that relies on being
+// within the constructor's closure.
+function Mapping(scale, accessor){
+
+  // We're going to need the current `this` object.
   var self = this
 
+  // Save the arguments to Mapping.
   self.scale = scale 
   self.accessor = accessor
 
-  //aliases for convenience
+  // Aliases for convenience-- the user calls `mapping.domain`, or
+  // `mapping.range`.
   self.domain = self.scale.domain
   self.range = self.scale.range
 
+  // `mapping.place` calls the accessor
+  //  and then passes its results through the scale. 
   self.place = function(data_point){
-    //the `this` object of functions called 
-    //    `selection.<method>(<arg1>, functions)` 
-    //is overwritten, hence this little dance with _place.
+    // D3 methods overwrite the `this` object, so, in order to maintain a
+    // reference to `mapping`, I reference `self` from  the enclosing
+    // scope.
     return self._place(this, data_point)
   } 
 
-  self.axis = null
+  // _axis_flag is a boolean to indicate whether we have already initialized an axis or not.
+  self._axis_flag = false
+  //it's common for the user to set max and min herself
 }
 
-var cons = Mapping
+
+// #### Bolierplate
+// I use consistent terms for the constructor and the prototype across all
+// Javascript class definitions.
+var cons =  Mapping
   , proto = cons.prototype
 
 proto.constructor = cons
 
+// ## Method definitions
+
+// #### mapping._place ###
+// Maps the data point into the screen space. The leading underscore reminds
+// users that they should use `mapping.place` over this method.
+
 proto._place = function(elem, data_point){
-  //maps the data point into the screen space
   return this.scale(this.accessor(data_point))
 }
 
+
+// ## Axis helpers
+
+// #### mapping.create_axis
+// Create an axis for this.scale.
+proto.create_axis = function(){
+  this.axis = d3.svg.axis()
+  this.axis.scale(this.scale)
+  return this.axis
+}
+
+
+// #### mapping.axis
+// a convenience function so the user only ever needs to call mapping.axis
+proto.axis = function(){
+  if(!this._axis_flag){
+    this.create_axis()
+  }
+  return this.axis()
+}
+
+
+// ## Domain helpers
+
+
+// #### mapping.min, mapping.max
+// Set the max or min element of the data domain.
+//
+// This function will compute the max if provided an array, simply set it if
+// provided with a single value, or if no argument is provided, it will return
+// the value of the current maximum.
+//
+// Returned value depends on the number of arguments passed thise functions:
+//
+// * *1 argument*: the mapping object
+// * *0 arguments*: the current bounds of the array
+//
+// max and min are essentially the same thing (modula the extrema), so the
+// heavy lifting is done by `bound`, defined below,
+
+proto.min(d){
+  return bound(d, {idx: 0, func: d3.min})
+}
+
+proto.max(d){
+  return bound(d, {idx: 1, func: d3.max})
+}
+
+// #### mapping.compute_domain
+// Computes the extent of the data dimension associated with self mapping.
+// If you specify `ordinal == true`, it will find the unique elements in the
+// values returned by `mapping.accessor` rather
+// than their extent.
+
 proto.compute_domain = function(data, ordinal){
-  //computes the extent of the data dimension associated with this mapping.
-  //spcify ordinal == true for ordinal data
-
-  var self = this
-    , points = data.map(self.accessor)
-
+  var self = self
+  var points = data.map(self.accessor)
   if (ordinal){
-    //find unique values
     self.domain(d3.set(points).values())
   } else {
-    //find max and min
     self.domain(d3.extent(points))
   }
   return self.scale
 }
 
-proto.create_axis = function(){
-  //create an axis for this scale
-  this.axis = d3.svg.axis()
-  this.axis.scale(this.scale)
-  return this.axis
+
+// #### bound 
+// Bound takes two arguments and impliments functionality described for
+// `mapping.min` and `mapping.max`.
+
+function bound(d, w){
+  var self = this
+    , domain = self.domain()
+
+  if(arguments.length = 0) {
+    return domain()[w.idx]
+  }
+
+  if(!isNaN(d.length)) {
+    domain[w.idx] = which.func(d, self.accessor)
+  } else {
+    domain[w.idx] = d
+  }
+
+  self.domain(domain)
+  return self
 }
+
